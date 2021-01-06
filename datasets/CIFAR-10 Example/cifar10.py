2bfc0bb680e1b696d1246369b1097d736b0394a6
umutykc <71348732+umutykc@users.noreply.github.com>
2021-01-06 19:40:05 +0300

    Update cifar10.py

--- datasets/CIFAR-10 Example/cifar10.py
+++ datasets/CIFAR-10 Example/cifar10.py
@@ -1,14 +1,20 @@
+#importing libs
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers, models
 import numpy as np
 import matplotlib.pyplot as plt
 
+#loading images and splitting into training and test images
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+
+#making image color based on 1
 train_images, test_images = train_images/255, test_images/255
 
+#creating classes
 classes = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 
+#creating model
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -19,14 +25,19 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 
+#showing model info
 model.summary()
 
+#prepearing model for compiling
 model.compile(optimizer="adam", loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"])
 
+#fitting model
 model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
+#making prediction
 prediction = model.predict(test_images)
 
+#visualization predictions
 for i in range(5):
     plt.grid(False)
     plt.imshow(test_images[i], cmap=plt.cm.binary)
