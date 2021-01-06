0d0ea37e478155fc3167f469871c39390efcd2fb
umutykc <71348732+umutykc@users.noreply.github.com>
2021-01-06 19:46:36 +0300

    Update mnist.py

--- datasets/MNIST Example/mnist.py
+++ datasets/MNIST Example/mnist.py
@@ -1,25 +1,34 @@
+#importing libs
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+#loading images and splitting into training and test images
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+#making image color based on 1
 train_images, test_images = train_images/255, test_images/255
 
+#creating classes
 classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+#creating model
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation="relu"),
     keras.layers.Dense(10, activation="softmax")
 ])
 
+#prepearing model for compiling
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
+#fitting model
 model.fit(train_images, train_labels, epochs=5)
 
+#making prediction
 prediction = model.predict(test_images)
 
+#visualization predictions
 for i in range(5):
     plt.grid(False)
     plt.imshow(test_images[i], cmap=plt.cm.binary)
